34347c53b4aebea4e068e279e4c1f71056f2c7e2
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2022-12-27 23:08:16 +0900

    fix: stash register value of right hand side on assignment

--- alang/src/codegen.c
+++ alang/src/codegen.c
@@ -105,9 +105,10 @@ void codegenAssignStmt(AstNode *n) {
   AstNode *right = n->child->brother;
 
   codegenExpr(right);
+  write_pseudo_inst_move(REG_V1, REG_V0);
   codegenReference(left);
 
-  write_inst_sw(REG_V0, REG_T0, 0);
+  write_inst_sw(REG_V1, REG_T0, 0);
 }
 
 void codegenReference(AstNode *n) {
